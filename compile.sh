d1da39698e2b028bc4e77be08b2c5bf86e1ac327
Marcel Valdez <marcel.valdez@live.com>
2015-10-26 19:23:03 -0400

    Remove all files before every compile

--- compile.sh
+++ compile.sh
@@ -3,18 +3,28 @@ TEST_DIR=$2
 OUTPUT_DIR=$3
 CLASSPATH=$4
 
-__compile() {
-  if [ ! -d "$OUTPUT_DIR" ]; then
-    mkdir "$OUTPUT_DIR"
+__clean() {
+  if [ -d "$OUTPUT_DIR" ]; then
+    working_dir=$(pwd)
+    cd "$OUTPUT_DIR"
+    rm -fr *
+    cd "$working_dir"
   fi
-
-  javac -classpath "$JAR_CLASSPATH:$OUTPUT_DIR" -d "$OUTPUT_DIR" $(find "$1" -iregex .*\.java)
 }
 
-# gather libararies
+__compile() {
+  if [ ! -d "$1" ]; then
+    mkdir "$1"
+  fi
+
+  javac -classpath "$JAR_CLASSPATH$OUTPUT_DIR" -d "$OUTPUT_DIR" $(find "$1" -iregex .*\.java)
+}
+
+# gather libraries, appends a : to the end
 JAR_CLASSPATH=$(find "$CLASSPATH" -iregex .*\.jar | tr '\n' ' ' |  sed 's/[ ][ ]*/:/g')
 # compile all source files
 if [ $? -eq 0 ]; then
+  __clean $OUTPUT_DIR
   # compile all test files
   __compile $SRC_DIR
   if [ $? -eq 0 ]; then
--- test.sh
+++ test.sh
@@ -2,11 +2,11 @@ OUTPUT_DIR=$1
 CLASSPATH=$2
 TEST_NAME=$3
 
-# compile all source files
+# compile all source files, appends a : to the end
 JAR_CLASSPATH=$(find "$CLASSPATH" -iregex .*\.jar | tr '\n' ' ' |  sed 's/[ ][ ]*/:/g')
 # run test
 if [ $? -eq 0 ]; then
-  java -classpath "$JAR_CLASSPATH:$OUTPUT_DIR" RunJunit "$OUTPUT_DIR" $TEST_NAME
+  java -classpath "$JAR_CLASSPATH$OUTPUT_DIR" RunJunit "$OUTPUT_DIR" $TEST_NAME
 fi
 
 exit $?
