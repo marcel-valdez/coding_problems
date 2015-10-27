293a62f6f8af2c0468e98ca2f7543b82db0d9bf6
Marcel Valdez <marcel.valdez@live.com>
2015-10-27 07:57:10 -0400

    Added new class with common behavior for all tests

---
+++ test/ChapterTestBase.java
@@ -0,0 +1,48 @@
+package test;
+
+import static org.hamcrest.Matcher.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.core.Is.*;
+import static org.hamcrest.core.IsNot.*;
+import static org.hamcrest.core.IsEqual.*;
+import static org.hamcrest.core.IsNull.*;
+
+
+import java.lang.*;
+import java.util.*;
+import java.util.stream.*;
+
+import org.hamcrest.*;
+import org.hamcrest.core.*;
+import org.junit.*;
+import org.junit.rules.*;
+import structures.*;
+
+public abstract class ChapterTestBase {
+  @Rule
+  public ErrorCollector errors = new ErrorCollector();
+
+  protected <T> void isTrueThat(String reason, T value, Matcher<T> matcher) {
+    errors.checkThat(reason, value, matcher);
+  }
+
+  protected <T> void isTrueThat(T value, Matcher<T> matcher) {
+    errors.checkThat(value, matcher);
+  }
+
+  protected <T> void isFalseThat(String reason, T value, Matcher<T> matcher) {
+    errors.checkThat(reason, value, not(matcher));
+  }
+
+  protected <T> void isFalseThat(T value, Matcher<T> matcher) {
+    errors.checkThat(value, not(matcher));
+  }
+
+  protected void areEqual(Object actual, Object expected) {
+    errors.checkThat(actual, equalTo(expected));
+  }
+
+  protected void areNotEqual(Object actual, Object expected) {
+    errors.checkThat(actual, not(equalTo(expected)));
+  }
+}
