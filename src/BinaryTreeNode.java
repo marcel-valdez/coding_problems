280bc351aaaf9452923ccb54b17c2fb11fb83fb4
Marcel Valdez <marcel.valdez@live.com>
2015-10-31 00:42:25 -0400

    Add binary tree

---
+++ src/BinaryTreeNode.java
@@ -0,0 +1,80 @@
+package structures;
+
+public class BinaryTreeNode<T> {
+  private final T value;
+  private BinaryTreeNode<T> left;
+  private BinaryTreeNode<T> right;
+  private BinaryTreeNode<T> parent;
+
+  public BinaryTreeNode(T value) { this.value = value; }
+
+  public T value() { return this.value; }
+
+  public void parent(BinaryTreeNode<T> parent) { this.parent = parent; }
+  public BinaryTreeNode<T> parent() { return this.parent; }
+
+  public void left(BinaryTreeNode<T> left) { this.left = left; }
+  public BinaryTreeNode<T> left() { return this.left; }
+
+  public void right(BinaryTreeNode<T> right) { this.right = right; }
+  public BinaryTreeNode<T> right() { return this.right; }
+
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append(String.valueOf(value));
+    if(left() != null) {
+      sb.append("\n");
+      toString(sb, left(), 0);
+    }
+
+    if(right() != null) {
+      sb.append("\n");
+      toString(sb, right(), 0);
+    }
+
+    return sb.toString();
+  }
+
+  public boolean equals(Object other) {
+    if(!(other instanceof BinaryTreeNode)) {
+      return false;
+    }
+
+    BinaryTreeNode node = (BinaryTreeNode) other;
+    return equal(this.value(), node.value())
+        && equal(this.left(), node.left())
+        && equal(this.right(), node.right());
+  }
+
+  private static boolean equal(Object a, Object b) {
+    if(a == null || b == null) { return a == b; }
+
+    return a.equals(b);
+  }
+
+  private static void toString(StringBuilder sb, BinaryTreeNode node, int parentLevel) {
+    appendLevels(sb, parentLevel);
+    if(node != null) {
+      sb.append("|-");
+      sb.append(String.valueOf(node.value));
+      if(node.left() != null) {
+        sb.append("\n");
+        toString(sb, node.left(), parentLevel + 1);
+      }
+
+      if(node.right() != null) {
+        sb.append("\n");
+        toString(sb, node.right(), parentLevel + 1);
+      }
+
+      if(node.right() != null || node.left() != null) {
+        sb.append("\n");
+        appendLevels(sb, parentLevel + 1);
+      }
+    }
+  }
+
+  private static void appendLevels(StringBuilder sb, int levels) {
+    for(int i = 0; i < levels; i++) { sb.append("| "); }
+  }
+}
