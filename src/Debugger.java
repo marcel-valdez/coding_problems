d616737f423d5c6ab55a1f71725dc87282010c5f
Marcel Valdez <marcel.valdez@live.com>
2015-11-10 23:27:18 -0500

    Improve debugger API to include conditional string formatting

--- src/Debugger.java
+++ src/Debugger.java
@@ -3,7 +3,7 @@ package debug;
 public final class Debugger {
   public static final Debugger DEBUG = new Debugger();
 
-  private boolean enabled = false;
+  private volatile boolean enabled = false;
 
   private Debugger() { }
 
@@ -15,12 +15,90 @@ public final class Debugger {
     return obj;
   }
 
+  public void println(String format, Object arg) {
+    if(!enabled) { return; }
+
+    println(format, new Object[] { arg });
+  }
+
+  public void println(String format, Object arg1, Object arg2) {
+    if(!enabled) { return; }
+
+    println(format,  new Object[] { arg1, arg2 });
+  }
+
+  public void println(String format, Object arg1, Object arg2, Object arg3) {
+    if(!enabled) { return; }
+
+    println(format,  new Object[] { arg1, arg2, arg3 });
+  }
+
+  public void println(String format, Object arg1, Object arg2, Object arg3, Object arg4) {
+    if(!enabled) { return; }
+
+    println(format,  new Object[] { arg1, arg2, arg3, arg4 });
+  }
+
+  public void println(String format, Object arg1, Object arg2, Object arg3, Object arg4, Object arg5) {
+    if(!enabled) { return; }
+
+    println(format,  new Object[] { arg1, arg2, arg3, arg4, arg5 });
+  }
+
+  public void println(String format, Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6) {
+    if(!enabled) { return; }
+
+    println(format,  new Object[] { arg1, arg2, arg3, arg4, arg5, arg6 });
+  }
+
+  public void println(String format, Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7) {
+    if(!enabled) { return; }
+
+    println(format,  new Object[] { arg1, arg2, arg3, arg4, arg5, arg6, arg7 });
+  }
+
+
+  public void println(String format, Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7, Object ... args) {
+    if(!enabled) { return; }
+
+    if(args == null) {
+      args = new Object[] { null };
+    }
+
+    Object[] allArgs = new Object[7 + args.length];
+    allArgs[0] = arg1;
+    allArgs[1] = arg2;
+    allArgs[2] = arg3;
+    allArgs[3] = arg4;
+    allArgs[4] = arg5;
+    allArgs[5] = arg6;
+    allArgs[6] = arg7;
+
+    if(args.length > 0) {
+      System.arraycopy(args, 0, allArgs, 7, args.length);
+    }
+
+    println(format,  allArgs);
+  }
+
+  private void println(String format, Object ... args) {
+    if(enabled) {
+      System.out.println(String.format(format, args));
+    }
+  }
+
   public <T> T print(T obj) {
     if(enabled) {
       System.out.print(obj + "");
     }
 
     return obj;
+  }
+
+  public void print(String format, Object ... args) {
+    if(enabled) {
+      System.out.print(String.format(format, args));
+    }
   }
 
   public String println(String msg) {
