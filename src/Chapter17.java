6ed042f0987f4e6fb2b7d54b844b8dd93a49bdd3
Marcel Valdez <marcel.valdez@appian.com>
2015-11-21 18:45:02 -0500

    Solve additional problems from chapter 17

--- src/Chapter17.java
+++ src/Chapter17.java
@@ -29,3 +29,213 @@ class PairOfNumbers {
     return new PairOfNumbers(a, b);
   }
 }
+
+class TicTacToeResultDetector {
+
+  // Assumptions all we want to know is if someone won not WHO won.
+  // the input is 1 for player 1, 2 for player 2, 0 for not used
+  public boolean detectWinner(byte[][] board) {
+    if(board == null || board.length != 3 || board[0].length != 3 ) {
+      throw new RuntimeException("Board can only by 3 by 3");
+    }
+
+    boolean diagonalA = true, diagonalB = true;
+    for(int i = 0; i <= 2; i++) {
+      if(board[i][0] != 0 && board[0][i] != 0) {
+        if ((board[i][0] == board[i][1] && board[i][1] == board[i][2])
+         || (board[0][i] == board[1][i] && board[1][i] == board[2][i])) {
+          return true;
+        }
+      }
+
+      if(i > 0) {
+        diagonalA &= board[i - 1][i - 1] == board[i][i];
+        diagonalB &= board[i - 1][3 - i] == board[i][2 - i];
+      }
+    }
+
+    return diagonalB || diagonalA;
+  }
+}
+
+class NFactorialTrailingZeros {
+  public int calculate(int n) {
+    if(n < 5) { return 0; }
+    if(n == 5) { return 1; }
+    if(n < 10) { return 1; }
+
+    int tens = n / 10;
+    int total = tens * 2;
+    if(n % 10 >= 5) {
+      total++;
+    }
+
+    return total;
+  }
+}
+
+class LargestSumSequenceFinder {
+  // Assumption: There is at least 1 positive number
+  public int find(int[] numbers) {
+    if(numbers == null || numbers.length == 0) {
+      throw new RuntimeException("Numbers cannot be null or empty");
+    }
+
+    if(numbers.length == 1) {
+      return numbers[0];
+    }
+
+    int maxSum = Integer.MIN_VALUE;
+    int currentSum = 0;
+
+    for(int number : numbers) {
+      int nextSum = currentSum + number;
+      // always check if adding the next number results in a max
+      // sequence sum, note that there is no need to check if the
+      // next number is negative before comparing against the max sum
+      if(nextSum > maxSum) {
+        maxSum = nextSum;
+      }
+
+      if(nextSum < 0) {
+        // restart the sequence sum if adding the next negative number
+        // results in a negative sum
+        currentSum = 0;
+      } else {
+        // so long as adding the next number does not result in a negative
+        // sum, it is worth considering it in the sequence sum
+        currentSum = nextSum;
+      }
+    }
+
+    return maxSum;
+  }
+}
+
+class WordCounter {
+  private static final int UPPER_LOWER_DELTA = 'a' - 'A';
+
+  public WordCounter() {}
+
+  public int count(String text, String word) {
+    String lowerWord = word.toLowerCase();
+
+    int comparedWordIndex = 0;
+    int count = 0;
+    boolean skipCurrentWord = false;
+    for(int i = 0; i < text.length(); i++) {
+      if(isNonLetter(text.charAt(i))) {
+        skipCurrentWord = false;
+        continue;
+      }
+
+      if(skipCurrentWord) {
+        continue;
+      }
+
+      if(isSameLetter(lowerWord.charAt(comparedWordIndex), text.charAt(i))) {
+          comparedWordIndex++;
+          if(comparedWordIndex == lowerWord.length()) {
+            if(i+1 == text.length() || isNonLetter(text.charAt(i+1))) {
+              // words matched!
+              count++;
+            }
+            // restart the word index
+            comparedWordIndex = 0;
+          }
+        } else {
+          comparedWordIndex = 0;
+          skipCurrentWord = true;
+        }
+    }
+
+    return count;
+  }
+
+  private boolean isSameLetter(char lowerCaseLetter, char otherLetter) {
+    return lowerCaseLetter == otherLetter ||
+           lowerCaseLetter == (otherLetter - UPPER_LOWER_DELTA);
+  }
+
+  private boolean isNonLetter(char symbol) {
+    return !(symbol >= 'a' && symbol <= 'z') &&
+           !(symbol >= 'A' && symbol <= 'Z');
+  }
+}
+
+class Pair {
+  private final int a;
+  private final int b;
+  public Pair(int a, int b) { this.a = a; this.b = b; }
+  public String toString() {
+    return String.format("(%s, %s)", a, b);
+  }
+}
+
+class PairFinder {
+  public ArrayList<Pair> findPairs(int[] numbers, int sum) {
+    if(numbers == null || numbers.length < 2) {
+      throw new RuntimeException("Numbers must be at least of length 2");
+    }
+
+    ArrayList<Pair> results = new ArrayList<>();
+    HashMap<Integer, Integer> numbersHash = new HashMap<>();
+
+    for(int number : numbers) {
+      if(numbersHash.containsKey(sum - number)) {
+        for(int i = 0; i < numbersHash.get(sum - number); i++) {
+          results.add(pair(number, sum - number));
+        }
+      }
+
+      if(numbersHash.containsKey(number)) {
+        numbersHash.put(number, numbersHash.get(number) + 1);
+      } else {
+        numbersHash.put(number, 1);
+      }
+    }
+
+    return results;
+  }
+
+  Pair pair(int a, int b) { return new Pair(a, b); }
+}
+
+class BinaryTreeToSortedList {
+  public void toSortedList(BiNode tree) {
+    ArrayList<BiNode> sortedNodes = new ArrayList<>();
+    sortedNodes.add(null);
+    appendToSortedList(tree, sortedNodes);
+    sortedNodes.add(null);
+
+    for(int i = 1; i < sortedNodes.size() - 1; i++) {
+      BiNode node = sortedNodes.get(i);
+      node.first(sortedNodes.get(i - 1));
+      node.second(sortedNodes.get(i + 1));
+    }
+  }
+
+  public void appendToSortedList(BiNode tree, List<BiNode> sortedNodes) {
+    if(tree == null) { return; }
+    appendToSortedList(tree.first(), sortedNodes);
+    sortedNodes.add(tree);
+    appendToSortedList(tree.second(), sortedNodes);
+  }
+}
+
+class BiNode {
+  private final int value;
+  private BiNode first;
+  private BiNode second;
+
+  public BiNode(int value) {
+    this.value = value;
+  }
+
+  public void first(BiNode first) { this.first = first; }
+  public BiNode first() { return this.first; }
+
+  public void second(BiNode second) { this.second = second; }
+  public BiNode second() { return this.second; }
+
+}
--- test/Chapter17Test.java
+++ test/Chapter17Test.java
@@ -35,4 +35,220 @@ public class Chapter17Test extends ChapterTestBase {
       areEqual(numbers.second, a[i]);
     }
   }
+
+  @Test
+  public void testTicTacToeResultDetector() {
+    checkBoard(new byte[][] {
+        { 1, 1, 1 },
+        { 0, 2, 0 },
+        { 2, 0, 2 }
+      },
+      true
+    );
+
+    checkBoard(new byte[][] {
+        { 0, 1, 1 },
+        { 2, 1, 0 },
+        { 2, 0, 2 }
+      },
+      false
+    );
+
+    checkBoard(new byte[][] {
+        { 0, 1, 2 },
+        { 2, 1, 0 },
+        { 2, 1, 2 }
+      },
+      true
+    );
+
+    checkBoard(new byte[][] {
+        { 1, 0, 0 },
+        { 2, 1, 0 },
+        { 2, 2, 1 }
+      },
+      true
+    );
+
+    checkBoard(new byte[][] {
+        { 0, 2, 0 },
+        { 2, 1, 0 },
+        { 1, 2, 1 }
+      },
+      false
+    );
+
+    checkBoard(new byte[][] {
+        { 0, 2, 1 },
+        { 2, 1, 0 },
+        { 1, 2, 0 }
+      },
+      true
+    );
+  }
+
+  private void checkBoard(byte[][] board, boolean expected) {
+    TicTacToeResultDetector detector = new TicTacToeResultDetector();
+
+    boolean actual = detector.detectWinner(board);
+
+    areEqual(toString(board), actual, expected);
+  }
+
+  private String toString(byte[][] board) {
+    StringBuilder sb = new StringBuilder(12);
+    sb.append("\n");
+
+    for(int i = 0; i < 3; i++) {
+      for(int j = 0; j < 3; j++) {
+        if(board[i][j] == 0) {
+           sb.append(" ");
+        } else {
+          if(board[i][j] == 1) { sb.append("X"); }
+          if(board[i][j] == 2) { sb.append("O"); }
+        }
+        if(j < 2) { sb.append("|"); }
+      }
+
+      sb.append("\n");
+      if(i < 2) { sb.append("-----\n"); }
+    }
+
+    return sb.toString();
+  }
+
+  @Test
+  public void testTrailingZerosCounter() {
+    NFactorialTrailingZeros target = new NFactorialTrailingZeros();
+    DEBUG.enable();
+    for(int n : new int[]
+        { 1, 4, 5, 6, 9, 10, 11, 14, 15, 16, 19, 20 }) {
+      long factorial = factorial(n);
+      areEqual(
+        format("For number %s (%s)", n, factorial),
+        target.calculate(n),
+        countTrailingZeros(factorial));
+    }
+  }
+
+  private int countTrailingZeros(long number) {
+    String str = number + "";
+    int counter = 0;
+    for(int i = str.length() - 1; i >= 0; i--) {
+      if(str.charAt(i) == '0') {
+        counter++;
+      } else {
+        break;
+      }
+    }
+
+    return counter;
+  }
+
+  private long factorial(int n) {
+    long result = 1;
+    for(long i = 1; i <= n; i++) {
+      result *= i;
+    }
+
+    return result;
+  }
+
+  @Test
+  public void testLargestSumSequenceFinder() {
+    checkLargestSumSequenceFinder(new int[] {3, -4, 9, 0, -8, 5, 2 }, 9);
+    checkLargestSumSequenceFinder(new int[] { -1  }, -1);
+    checkLargestSumSequenceFinder(new int[] { -1, -2, 0 }, 0);
+    checkLargestSumSequenceFinder(new int[] { -4, -2, -1, -3 }, -1);
+    checkLargestSumSequenceFinder(new int[] { 1, 2, 3}, 6);
+    checkLargestSumSequenceFinder(new int[] { 4, -2, 3}, 5);
+    checkLargestSumSequenceFinder(new int[] { 1, -5, 4, -2, 3, -2 }, 5);
+  }
+
+  private void checkLargestSumSequenceFinder(int[] numbers, int expectedMaxSum) {
+    LargestSumSequenceFinder target = new LargestSumSequenceFinder();
+    // when
+    int actualMaxSum = target.find(numbers);
+    // then
+    areEqual(actualMaxSum, expectedMaxSum);
+  }
+
+  @Test
+  public void testWordCounter() {
+    // given
+    String text = "My mother lives with her mother at her mother's house, with a " +
+                  "motherload of stuff my mother's room, at Mothers City";
+    String word = "Mother";
+    int expectedCount = 4;
+    WordCounter target = new WordCounter();
+    // when
+    int actualCount = target.count(text, word);
+    // then
+    areEqual(actualCount, expectedCount);
+  }
+
+  @Test
+  public void testPairFinder() {
+    // given
+    int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 1, 13 };
+    PairFinder target = new PairFinder();
+    // when
+    ArrayList<Pair> pairs = target.findPairs(numbers, 6);
+    // then
+    // DEBUG.enable();
+    DEBUG.println(pairs);
+    areEqual(pairs.size(), 4);
+
+    // when
+    pairs = target.findPairs(numbers, 2);
+    // then
+    DEBUG.println(pairs);
+    areEqual(pairs.size(), 2);
+
+    // when
+    pairs = target.findPairs(numbers, 3);
+    // then
+    DEBUG.println(pairs);
+    areEqual(pairs.size(), 3);
+  }
+
+  @Test
+  public void testBinaryTreeToSortedList() {
+    // given
+    BiNode five = binode(5);
+    BiNode three = binode(3);
+    BiNode four = binode(4);
+    BiNode two = binode(2);
+    BiNode one = binode(1);
+    BiNode six = binode(6);
+    BiNode seven = binode(7);
+    BiNode eight = binode(8);
+    BiNode nine = binode(9);
+
+    five.first(three); five.second(seven);
+
+    three.first(two); three.second(four);
+    seven.first(six); seven.second(eight);
+
+    two.first(one); eight.second(nine);
+
+    BinaryTreeToSortedList target = new BinaryTreeToSortedList();
+    // when
+    target.toSortedList(five);
+
+    // then
+    areEqual(one.second(), two); areEqual(one.first(), null);
+    areEqual(two.second(), three); areEqual(two.first(), one);
+    areEqual(three.second(), four); areEqual(three.first(), two);
+    areEqual(four.second(), five); areEqual(four.first(), three);
+    areEqual(five.second(), six); areEqual(five.first(), four);
+    areEqual(six.second(), seven); areEqual(six.first(), five);
+    areEqual(seven.second(), eight); areEqual(seven.first(), six);
+    areEqual(eight.second(), nine); areEqual(eight.first(), seven);
+    areEqual(nine.second(), null); areEqual(nine.first(), eight);
+  }
+
+  BiNode binode(int value) {
+    return new BiNode(value);
+  }
 }
