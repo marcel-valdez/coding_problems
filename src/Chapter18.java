5bdad11ed1bc8cf8e8d21f23b1f9bf714f084704
Marcel Valdez <marcel.valdez@live.com>
2015-12-31 04:16:06 +0000

    Solve word distance problem. Chapter 18.

--- src/Chapter18.java
+++ src/Chapter18.java
@@ -5,6 +5,7 @@ import static debug.Debugger.DEBUG;
 import java.lang.*;
 import java.lang.reflect.*;
 import java.util.*;
+import java.util.function.*;
 import java.util.stream.*;
 
 import structures.*;
@@ -44,27 +45,135 @@ class Shuffler {
 
     return shuffled;
   }
-  
-  class RandomSetGenerator {
-    // Assumption: source length must be greater than or equal to output size
-    // Assumption: No repeated numbers are desired
-    public Set<Integer> generate(int[] source, int outputSize) {
-      Random random = new Random(System.nanoTime());
-      List<Integer> indices = new ArrayList<>();
-      // con: requires O(N) memory
-      for(int i = 0; i < source.length; i++) {
-        indices.add(i);
-      }
+}
 
-      Set<Integer> result = new HashSet<>();
-      for(int i = 0; i < outputSize; i++) {
-        int indicesIndex = random.nextInt(indices.size());
-        int sourceIndex = indices.get(indicesIndex);
-        result.add(source[sourceIndex]);
-        indices.remove(indicesIndex);
-      }
-
-      return result;
+class RandomSetGenerator {
+  // Assumption: source length must be greater than or equal to output size
+  // Assumption: No repeated numbers are desired
+  public Set<Integer> generate(int[] source, int outputSize) {
+    Random random = new Random(System.nanoTime());
+    List<Integer> indices = new ArrayList<>();
+    // con: requires O(N) memory
+    for(int i = 0; i < source.length; i++) {
+      indices.add(i);
     }
+
+    Set<Integer> result = new HashSet<>();
+    for(int i = 0; i < outputSize; i++) {
+      int indicesIndex = random.nextInt(indices.size());
+      int sourceIndex = indices.get(indicesIndex);
+      result.add(source[sourceIndex]);
+      indices.remove(indicesIndex);
+    }
+
+    return result;
+  }
+}
+
+class TwosCounter {
+  public int countTwos(int n) {
+    if(n < 2) {
+      return 0;
+    }
+
+    if(n < 10) {
+      return 1;
+    }
+
+    int count = 1; // number 2
+    double divisionResult = n / 10.0;
+    while(divisionResult > 0) {
+      count += divisionResult;
+      double nextDivisionResult = divisionResult / 10.0;
+      count += (int) nextDivisionResult;
+      double remainder = nextDivisionResult - ((int) nextDivisionResult);
+      if(remainder >= 0.2) {
+        count++;
+      }
+
+      divisionResult = nextDivisionResult;
+    }
+
+    return count;
+  }
+}
+
+// given a large file with a lot of words, find the shortest distance between
+// any two words. If the operation will be repeated many times for the same
+// file with other pairs of words, can you optimize your solution?
+
+class WordDistanceFinder {
+
+  public int generalFind(String[] document, String wordA, String wordB) {
+    int lastAIdx = -1;
+    int lastBIdx = -1;
+    int minDistance = Integer.MAX_VALUE;
+
+    for(int i = 0; i < document.length; i++) {
+      // A C B D E A
+      if(wordA.equals(document[i])) {
+        lastAIdx = i;
+      }
+
+      if(wordB.equals(document[i])) {
+        lastBIdx = i;
+      }
+
+      if(lastAIdx >= 0 && lastBIdx >= 0) {
+        int distance = Math.abs(lastAIdx - lastBIdx);
+        if(distance < minDistance) {
+          minDistance = distance;
+        }
+      }
+    }
+
+    if(minDistance == Integer.MAX_VALUE) {
+      return -1;
+    }
+
+    return minDistance;
+  }
+
+  public BiFunction<String, String, Integer> optimizedFind(String[] document) {
+    final Map<String, List<Integer>> wordsIndices = new HashMap<>();
+    for(int i  = 0; i < document.length; i++) {
+      String word = document[i];
+      List<Integer> wordIndices = wordsIndices.get(word);
+      if(wordIndices == null) {
+        wordIndices = new ArrayList<>();
+        wordsIndices.put(word, wordIndices);
+      }
+
+      wordIndices.add(i);
+    }
+
+    return (String wordA, String wordB) -> {
+      List<Integer> wordAIndices = wordsIndices.get(wordA);
+      List<Integer> wordBIndices = wordsIndices.get(wordB);
+
+      if(wordAIndices == null || wordBIndices == null) {
+        return -1;
+      }
+
+      int minDistance = Integer.MAX_VALUE;
+      int aCounter = 0;
+      int bCounter = 0;
+      while(aCounter < wordAIndices.size() && bCounter < wordBIndices.size()) {
+        int aIndex = wordAIndices.get(aCounter);
+        int bIndex = wordBIndices.get(bCounter);
+        int distance = Math.abs(aIndex - bIndex);
+        if(distance < minDistance) {
+          minDistance = distance;
+        }
+
+        if(aIndex < bIndex) {
+          aCounter++;
+        } else {
+          bCounter++;
+        }
+      }
+
+      return minDistance;
+    };
   }
 }
--- test/Chapter18Test.java
+++ test/Chapter18Test.java
@@ -11,6 +11,7 @@ import static org.hamcrest.core.IsNull.*;
 
 import java.lang.*;
 import java.util.*;
+import java.util.function.*;
 import java.util.stream.*;
 
 import org.hamcrest.core.*;
@@ -34,5 +35,55 @@ public class Chapter18Test extends ChapterTestBase {
     // then
     Assert.assertEquals(cards.length, shuffledCards.length);
     Assert.assertThat(Arrays.asList(shuffledCards), hasItems(shuffledCards));
+  }
+
+  @Test
+  public void testTwosCounter() {
+    // given
+    int n = 123;
+    int expected = (new int[] { 2, 12, 22, 22, 32, 42, 52, 62, 72, 82, 92, 102, 112, 120, 122, 122  }).length;
+    TwosCounter counter = new TwosCounter();
+    // when
+    int actual = counter.countTwos(n);
+    // then
+    Assert.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testGeneralWordDistanceFinder() {
+    // given
+    final String[] words = { "A", "B", "C", "D", "B", "E", "A", "D", "F", "C", "A", "G", "C" };
+    final WordDistanceFinder target = new WordDistanceFinder();
+    checkWordDistanceFinder((a, b) -> target.generalFind(words, a, b));
+  }
+
+  @Test
+  public void testOptimizedWordDistanceFinder() {
+    // given
+    final String[] words = { "A", "B", "C", "D", "B", "E", "A", "D", "F", "C", "A", "G", "C" };
+    final WordDistanceFinder target = new WordDistanceFinder();
+    checkWordDistanceFinder(target.optimizedFind(words));
+  }
+
+  private void checkWordDistanceFinder(BiFunction<String, String, Integer> finder) {
+    Assert.assertEquals("AD", 1, finder.apply("A", "D").intValue());
+    Assert.assertEquals("DA", 1, finder.apply("D", "A").intValue());
+
+    Assert.assertEquals("BD", 1, finder.apply("B", "D").intValue());
+    Assert.assertEquals("DB", 1, finder.apply("D", "B").intValue());
+
+    Assert.assertEquals("EB", 1, finder.apply("E", "B").intValue());
+    Assert.assertEquals("BE", 1, finder.apply("B", "E").intValue());
+
+    Assert.assertEquals("ED", 2, finder.apply("E", "D").intValue());
+    Assert.assertEquals("DE", 2, finder.apply("D", "E").intValue());
+
+    Assert.assertEquals("BG", 7, finder.apply("B", "G").intValue());
+    Assert.assertEquals("GB", 7, finder.apply("G", "B").intValue());
+
+    Assert.assertEquals("BZ", -1, finder.apply("B", "Z").intValue());
+    Assert.assertEquals("ZB", -1, finder.apply("Z", "B").intValue());
+
+    Assert.assertEquals(-1, finder.apply("Z", "T").intValue());
   }
 }
